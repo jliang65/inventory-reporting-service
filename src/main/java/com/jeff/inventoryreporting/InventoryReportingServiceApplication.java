1b0e6dc6e58f2a7c0bab42379c2985303827a87a
Jeff <197541511+jliang65@users.noreply.github.com>
2026-07-29 21:19:37 -0500

    Initial commit

---
+++ src/main/java/com/jeff/inventoryreporting/InventoryReportingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.inventoryreporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryReportingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryReportingServiceApplication.class, args);
+	}
+
+}
